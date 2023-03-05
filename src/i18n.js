a10ec55004212c052a1057867eea7703591ac6d6
turulomio <turulomio@yahoo.es>
2023-03-05 08:13:35 +0100

    i18n works

--- src/i18n.js
+++ src/i18n.js
@@ -1,20 +1,21 @@
 // import Vue from 'vue'
-import VueI18n from 'vue-i18n'
+import { createI18n } from 'vue-i18n'
+import json_es from './locales/es.json'
 
 // Vue.use(VueI18n)
 
-function loadLocaleMessages () {
-  const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
-  const messages = {}
-  locales.keys().forEach(key => {
-    const matched = key.match(/([A-Za-z0-9-_]+)\./i)
-    if (matched && matched.length > 1) {
-      const locale = matched[1]
-      messages[locale] = locales(key)
-    }
-  })
-  return messages
-}
+// function loadLocaleMessages () {
+//   const locales = require.context('./locales', true, /[A-Za-z0-9-_,\s]+\.json$/i)
+//   const messages = {}
+//   locales.keys().forEach(key => {
+//     const matched = key.match(/([A-Za-z0-9-_]+)\./i)
+//     if (matched && matched.length > 1) {
+//       const locale = matched[1]
+//       messages[locale] = locales(key)
+//     }
+//   })
+//   return messages
+// }
 
 // export default new VueI18n({
 //   locale: process.env.VUE_APP_I18N_LOCALE || 'en',
@@ -26,10 +27,15 @@ function loadLocaleMessages () {
 
 
 // 2. Create i18n instance with options
-const i18n = VueI18n.createI18n({
-  locale: 'ja', // set locale
+const i18n=createI18n({
+  locale: 'es', // set locale
   fallbackLocale: 'en', // set fallback locale
-  messages:loadLocaleMessages(), // set locale messages
+  messages:{
+    es:json_es,
+  }
+  // messages:loadLocaleMessages(), // set locale messages
   // If you need to specify other options, you can set other options
   // ...
 })
+
+export default i18n
